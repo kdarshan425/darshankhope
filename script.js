e5214402d04df7d5ebaafb03fac1371b0af3570b
kdarshan425 <64018424+kdarshan425@users.noreply.github.com>
2020-09-17 21:48:21 +0530

    done
    done

--- script.js
+++ script.js
@@ -6,7 +6,7 @@
 //type
 $('document').ready(function(){
             var typed=new Typed('#effect',{
-        strings:["WEB DEVELOPER!","PROGRAMMER!", "FREE LANCER !","ENTC ENGINEER!"],
+        strings:["WEB DEVELOPER!","PROGRAMMER!","FREE LANCER!","ENTC ENGINEER!"],
         backSpeed:50,
         typeSpeed:50,
         loop:true,
