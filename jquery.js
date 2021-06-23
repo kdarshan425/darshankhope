8f9385602e6b64279a44e877a1eda51e5e5b422b
Darshan Subhash Khope <64018424+kdarshan425@users.noreply.github.com>
2021-06-23 15:57:40 +0530

    Update jquery.js

--- jquery.js
+++ jquery.js
@@ -5,6 +5,7 @@
  */
 
 
+
 $('.owl-carousel').owlCarousel({
     loop:true,
     margin:18,
@@ -16,10 +17,10 @@ $('.owl-carousel').owlCarousel({
             items:1
         },
         600:{
-            items:1
+            items:3
         },
         1000:{
-            items:1
+            items:3
         }
     }
 })
